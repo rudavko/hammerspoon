65fe47daa934de9cbb055581169bac51f4a5f9fb
Andrii <Rudavko@gmail.com>
2019-10-02 17:07:30 +0300

    Init conmmit

---
+++ hyper-apps-defaults.lua
@@ -0,0 +1,17 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'a', 'Agenda' },            
+  { 'c', 'Google Chrome' },     
+  { 'v', 'Visual Studio Code' },
+  { 'f', 'Finder' },            
+  { 'm', 'Mail' },       
+  { 'h', 'Hyper' },      
+  { 'o', 'OmniFocus' },      
+  { 'g', 'GitHub Dektop' },      
+  { 's', 'Safari' },        
+  { 'j', 'Jump Desktop' },        
+}
